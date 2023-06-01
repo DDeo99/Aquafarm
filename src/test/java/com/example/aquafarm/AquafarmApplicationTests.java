08f732fc2e48d65a20324658298988d234c5797b
DW <yoondukwoo@gmial.com>
2023-06-01 18:17:33 +0900

    Springboot Initialize

---
+++ src/test/java/com/example/aquafarm/AquafarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquafarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquafarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
