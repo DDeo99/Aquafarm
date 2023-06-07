c5507e93298453f40dd702b8336ab7cfe76daeb7
DW <yoondukwoo@gmial.com>
2023-06-08 04:41:09 +0900

    @CrossOrigin 위치 수정

--- src/main/java/com/example/aquafarm/Fish/Controller/FishInfoController.java
+++ src/main/java/com/example/aquafarm/Fish/Controller/FishInfoController.java
@@ -4,12 +4,14 @@ import com.example.aquafarm.Fish.DTO.FishInfoDTO;
 import com.example.aquafarm.Fish.Domain.FishInfo;
 import com.example.aquafarm.Fish.Service.FishInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/fish-info")
 public class FishInfoController {
