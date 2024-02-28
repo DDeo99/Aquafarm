32e6f9d93708f8107c127db0c34bc94c2b085ec4
DDeo99 <yoondukwoo@gmail.com>
2024-02-28 14:09:42 +0900

    remove: Useless files

--- src/main/java/com/example/aquafarm/Aquafarm/Repository/AquafarmInfoRepository.java
+++ src/main/java/com/example/aquafarm/Aquafarm/Repository/AquafarmInfoRepository.java
@@ -10,20 +10,10 @@ import java.util.Optional;
 @Repository
 public interface AquafarmInfoRepository extends JpaRepository<AquafarmInfo, Integer> {
     List<AquafarmInfo> findByAddress(String address);
-    // address로 AquafarmInfo를 조회하는 메소드
-
     List<AquafarmInfo> findByUserId(int userId);
-    // userId로 AquafarmInfo를 조회하는 메소드
-
     List<AquafarmInfo> findByAddressContaining(String keyword);
-
     List<AquafarmInfo> findByCustomCriteria(String criteria);
-
     Optional<AquafarmInfo> findById(int aquafarmId);
-
-    //AquafarmInfo findById(int aquafarmId);
-    // 특정 키워드를 포함하는 address를 가진 AquafarmInfo를 조회하는 메소드
-
     Optional<AquafarmInfo> findFirstByOrderByAquafarmId();
 
 }
